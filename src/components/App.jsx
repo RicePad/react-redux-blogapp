acd68c057a5b30aaa58aa7c8498b753844195779
RiskyClick <jonn.thinkrtc@gmail.com>
2017-08-20 05:44:40 +0000

    use moment.js framework to display how many days left for each reminder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
-// import { addReminder } from '../actions';
-
 import { addReminder,  deleteReminder } from '../actions';
+import moment from 'moment';
+
+
 
 class App extends Component {
     constructor(props){
@@ -43,7 +44,11 @@ class App extends Component {
                         
                         return(
                              <li key={reminder.id} className="list-group-item">
-                                <div className="list-item"> { reminder.text }</div>
+                                <div className="list-item">
+                                    <div> { reminder.text }</div>
+                                    <div><em>{moment( new Date(reminder.dueDate)).fromNow()}</em></div>
+
+                                </div>
                                 <div className="list-item delete-button"
                                      onClick={() => this.deleteReminder(reminder.id)}
 
