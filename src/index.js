d5e7a286944cbd06fc28ca1a8a77e8d82aecca9d
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-05 04:45:46 +0000

    create action to pull data from Blog ApI

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDom from 'react-dom';
 import PostIndex from './components/PostIndex'
 import PostsShow from './components/PostsShow'
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
 import reducers from './reducers';
+
+import promise from 'redux-promise'
 
 
 import { Router, Route, browserHistory } from 'react-router';
 
-const store = createStore(reducers);
+const store = applyMiddleware()(promise)(createStore);
+
+
+//  *** TEST APPLY MIDDLEWARE COMMENTING THIS SECTINO OUT.
+// const store = createStore(reducers);
 
 
 ReactDom.render(
-  <Provider store={ store }> 
+  <Provider store={ store(reducers) }> 
     <Router path="/" history={browserHistory}>
         <Route path="/index" component={PostIndex} />
         <Route path="/blog" component={PostsShow}/>
