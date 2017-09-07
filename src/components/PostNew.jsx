5b3afe98ce15d4a7915134f75811da6bbf397266
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-07 16:22:35 +0000

    add touched trigered error message

--- src/components/PostNew.jsx
+++ src/components/PostNew.jsx
@@ -5,22 +5,80 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component{
     
+    renderField(field){
+        return(
+            <div className="form-group has-danger">
+            <label>{ field.label }</label>
+                <input
+                    {...field.input } 
+                    className="form-control"
+                    placeholder={field.placeholder}
+                
+                />
+                <div className="text-help">
+                    {field.meta.touched ? field.meta.error : '' }
+                </div>
+            </div>
+            );
+        
+    }
+    
+    
+    onSubmit(values){
+        console.log(values);
+    }
     
     render(){
+        
+        const { handleSubmit } = this.props;
         return(
-            <div> 
-            
-                Create your first posts
-                <div><Link to={'/'}>Go back to list of blogs</Link></div>
-            </div>
+           <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                    placeholder="add title"
+                />
+                <Field 
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                    placeholder="add tags"
+    
+                />
+                <Field 
+                    label="Content"
+                    name="content"
+                    component={this.renderField}
+                    placeholder="add content"
+    
+                    
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+               
+           </form>
             );
         
     }
 }
 
-
-
+function validate(values){
+    const errors={};
+    
+    if(!values.title){
+        errors.title = "Enter a title";
+     }
+    if(!values.categories) {
+        errors.categories = "Enter some categories";   
+    }if(!values.content){
+        errors.content = "Enter some content please";
+    }
+    
+    return errors;
+    
+}
 export default reduxForm({
+    validate: validate,
     form: 'PostsNewForm'
     
 })(PostNew);
